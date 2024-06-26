5d80eaacdc4db62aef662b32a45379b1080f55c3
vitt <fivtin@mail.ru>
2024-06-27 01:49:54 +0300

    project setup 3

--- mailer/models.py
+++ mailer/models.py
@@ -1,3 +1,78 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Client(models.Model):
+
+    email = models.EmailField(max_length=256, unique=True, verbose_name='e-mail')
+    fio = models.CharField(max_length=128, verbose_name='ф.и.о.')
+    info = models.TextField(verbose_name='комментарий')
+
+    class Meta:
+        verbose_name = 'клиент'
+        verbose_name_plural = 'клиенты'
+        # ordering = ('id',)
+
+    def __str__(self):
+        return f'{self.email} ({self.fio})'
+
+
+class Message(models.Model):
+
+    subject = models.CharField(max_length=256, verbose_name='тема')
+    body = models.TextField(verbose_name='содержимое')
+
+    class Meta:
+        verbose_name = 'сообщение'
+        verbose_name_plural = 'сообщений'
+
+    def __str__(self):
+        return self.subject
+
+
+class Newsletter(models.Model):
+    FREQUENCE_CHOICES = {
+        (1, "dayly"),
+        (2, "weekly"),
+        (3, "monthly"),
+    }
+    STATUS_CHOICES = {
+        (1, "created"),
+        (2, "launched"),
+        (3, "completed"),
+    }
+    title = models.CharField(max_length=128, verbose_name='название')
+    start = models.DateTimeField(verbose_name='время начала')
+    finish = models.DateTimeField(verbose_name='время окончания')
+    frequency = models.PositiveSmallIntegerField(choices=FREQUENCE_CHOICES, default=1, verbose_name='периодичность')
+    status = models.PositiveSmallIntegerField(choices=STATUS_CHOICES, default=1, verbose_name='статус')
+    message = models.ForeignKey(Message, on_delete=models.CASCADE)
+    clients = models.ManyToManyField(Client)
+
+    class Meta:
+        verbose_name = 'рассылка'
+        verbose_name_plural = 'рассылки'
+
+    def __str__(self):
+        return f'{self.title}({self.status}): {self.start}-{self.frequency}'
+
+
+class Recipient(models.Model):
+    newsletter = models.ForeignKey(Newsletter, on_delete=models.CASCADE, verbose_name='рассылка')
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, verbose_name='клиент')
+
+    class Meta:
+        verbose_name = 'получатель'
+        verbose_name_plural = 'получатели'
+
+    def __str__(self):
+        return f'{self.newsletter.title}: {self.client.email}'
+
+
+class Attempt(models.Model):
+    newsletter = models.ForeignKey(Newsletter, on_delete=models.CASCADE, verbose_name='рассылка')
+    attempt_time = models.DateTimeField(auto_now_add=True, verbose_name='время попытки')
+    success = models.BooleanField(default=False, verbose_name='успешно')
+    response = models.TextField(verbose_name='ответ сервера')
+
--- mailer/urls.py
+++ mailer/urls.py
@@ -2,12 +2,15 @@ from django.urls import path
 from django.views.decorators.cache import cache_page
 
 from mailer.apps import MailerConfig
+from mailer.views import index
+
 # from catalog.views import ProductListView, ProductDetailView, ContactsTemplateView, ProductCreateView, \
 #     ProductUpdateView, ProductDeleteView, CategoryListView
 
 app_name = MailerConfig.name
 
 urlpatterns = [
+    path('', index),
     # path('', ProductListView.as_view(), name='product_list'),
     # path('category/', CategoryListView.as_view(), name='category_list'),
     # path('product/<int:pk>', cache_page(60)(ProductDetailView.as_view()), name='product_detail'),
