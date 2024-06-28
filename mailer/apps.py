e0fe4db67373bb597a9c9178188abc2d41a04db7
fivtin <fivtin@mail.ru>
2024-06-28 17:52:43 +0300

    small modifies

--- mailer/apps.py
+++ mailer/apps.py
@@ -4,3 +4,10 @@ from django.apps import AppConfig
 class MailerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'mailer'
+
+    """
+    def ready(self):
+         from имя_приложения.модуль_с_задачей import функция_старта 
+         sleep(2)
+         функция_старта()
+    """
